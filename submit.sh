7095451b522180c4110fc72a6f715ddcf8827ffb
Dendi Suhubdy <suhubdyd@iro.umontreal.ca>
2017-12-07 10:55:21 -0500

    adjusting to custom script at argv[1]

--- submit.sh
+++ submit.sh
@@ -6,29 +6,32 @@ echo "	           MULTIPLE ON SLURM CLUSTER               "
 echo "-----------------------------------------------------"
 echo "starting infinite loop for submitting jobs"
 
+SCRIPT=${1}
+
 while :
 do
 	echo "number of unkillable jobs : "
 	UNKILL=$(squeue --user=suhubdyd | grep -c unkillable)
 	if [ $UNKILL==0 ]; then
 		echo $UNKILL
-		sbatch --qos=unkillable --gres=gpu --mem=20000 ./main.sh
+		sbatch --qos=unkillable --gres=gpu --mem=20000 ${SCRIPT}
 	fi
 	echo "number of high jobs : " 
 	HIGH=$(squeue --user=suhubdyd | grep -c high)
 	if [ $HIGH == 0 ]; then
 		echo $HIGH
-		sbatch --qos=high --gres=gpu --mem=20000 ./main.sh
-		sbatch --qos=high --gres=gpu --mem=20000 ./main.sh
+		sbatch --qos=high --gres=gpu --mem=20000 ${SCRIPT}
+		sbatch --qos=high --gres=gpu --mem=20000 ${SCRIPT}
 	fi
 	echo "number of low jobs : " 
 	LOW=$(squeue --user=suhubdyd | grep -c low)
 	if [ $LOW == 0 ]; then 
 		echo $LOW
-		sbatch --qos=low --gres=gpu --mem=20000 ./main.sh
-		sbatch --qos=low --gres=gpu --mem=20000 ./main.sh
-		sbatch --qos=low --gres=gpu --mem=20000 ./main.sh
-		sbatch --qos=low --gres=gpu --mem=20000 ./main.sh
+		sbatch --qos=low --gres=gpu --mem=20000 ${SCRIPT}
+		sbatch --qos=low --gres=gpu --mem=20000 ${SCRIPT}
+		sbatch --qos=low --gres=gpu --mem=20000 ${SCRIPT}
+		sbatch --qos=low --gres=gpu --mem=20000 ${SCRIPT}
+		sbatch --qos=low --gres=gpu --mem=20000 ${SCRIPT}
 	fi
 	echo "Another call.."
 	# wait for another half an hour
